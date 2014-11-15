d6684bfbc4ad0a3a7ce7a9a7ed1f29fefa439303
Brandon Lind <linddeveloper@gmail.com>
2014-11-15 16:40:52 +0000

    updating node/express to use a host. Centering header/footer.

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var http = require('http'),
     express = require('express'),
     app = express(),
-    apiServer = require('./api/server.js');
-    
-var port = process.env.PORT || 8080;
-http.createServer(app).listen(port);
-console.log('Listening on port ' + port);
+    apiServer = require('./api/server.js'),
+    port = process.env.PORT || 8080,
+    host = process.env.IP || '0.0.0.0';
+
+http.createServer(app).listen(port, host);
+console.log('HTTP listening at ' + host + ' on port ' + port);
 
 app.use('/api', apiServer); // Mount the HTTP API on the URL space /api
 app.use(express.static(__dirname + '/static')); // For other requests, just serve /static
